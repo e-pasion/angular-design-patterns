857261176fa65efc8a2049aa5d40add1c930764d
elias pasion <elias.pasion@pragma.com.co>
2024-04-14 13:43:57 -0500

    decorator

---
+++ estructurales/decorator/component.ts
@@ -0,0 +1,28 @@
+interface Component {
+    operation() :string;
+}
+
+class ConcreteComponent implements Component{
+    operation(): string {
+        return 'ConcreteComponent';
+    }
+}
+
+class Decorator implements Component {
+    constructor(protected component:Component){}
+    operation(): string {
+        return this.component.operation();
+    }
+
+}
+
+class ComponentDecoratorA extends Decorator {
+    public operation():string {
+        return `ComponentDecoratorA${super.operation()}`
+    }
+}
+
+const component = new ConcreteComponent();
+const decorator= new ComponentDecoratorA(component);
+
+console.log(decorator.operation());
